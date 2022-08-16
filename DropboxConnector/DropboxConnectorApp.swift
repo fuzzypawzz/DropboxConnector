e61024a8b0ddd00ec8e9c6ee52c873a407ffe32a
Jannik Maag <jannikmaag@outlook.com>
2022-08-16 21:16:06 +0200

    Initial Commit

---
+++ DropboxConnector/DropboxConnectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropboxConnectorApp.swift
+//  DropboxConnector
+//
+//  Created by Jannik Maag on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct DropboxConnectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
